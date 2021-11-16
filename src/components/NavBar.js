9d4d339c315f103c809e0b6f291e7b1ded851428
Lucas <lucasponti22@gmail.com>
2021-11-16 12:08:58 -0300

    Modified

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css';
+import liplogo from './liplogo.png'
+const NavBar = () => {
+    return (
+        <nav className='miClase'>
+            <div id='navigation' className='container'>
+                <a href='index.html'><img src={liplogo} alt="Logotipo"/></a>
+            
+                <ul id="main-nav">    
+                    <li><a className='btn' href=''>Productos</a></li>
+                    <li><a className='btn' href=''>Ofertas</a></li>
+                    <li><a className='btn' href=''>Otros</a></li>
+                </ul>
+                <a className='contacto' href=''>Contacto</a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
