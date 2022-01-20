f360d595e7961e64bd1d518ac4129bb8b5c382ac
Lucas <lucasponti22@gmail.com>
2022-01-19 21:24:15 -0300

    ENTREGA FINAL

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,18 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6ts0gJ_YZ6iaeSqoY24uYvPwrRBwTxzM" ,
-  authDomain: "coderhouse-lipecommerce.firebaseapp.com" ,
-  projectId: "coderhouse-lipecommerce" ,
-  storageBucket: "coderhouse-lipecommerce.appspot.com" ,
-  messagingSenderId: "540317955634" ,
-  appId: "1:540317955634:web:21ee260c94d9e8cbe02e2b"
-  // apiKey: process.env.REACT_APP_apiKey,
-  // authDomain: process.env.REACT_APP_authDomain,
-  // projectId: process.env.REACT_APP_projectId,
-  // storageBucket: process.env.REACT_APP_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig)
